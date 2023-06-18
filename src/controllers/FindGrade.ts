472cf846d6f2d81949873ff9dd85a22c67e70676
Jose Neto <joseneto5689@pop-os.localdomain>
2023-06-18 16:16:17 -0300

    More errors fixed

--- src/controllers/FindGrade.ts
+++ src/controllers/FindGrade.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import prismaClient from "../database";
-import { error } from "console";
 
 export class FindGradeController {
     async handle(request: Request, response: Response) {
