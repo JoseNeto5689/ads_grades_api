0c76b6a3883b214a8f849a8d3008c16088af616f
Jose Neto <joseneto5689@pop-os.localdomain>
2023-06-18 15:53:55 -0300

    Fixing errors

--- src/controllers/FindGrade.ts
+++ src/controllers/FindGrade.ts
@@ -1,17 +1,22 @@
 import { Request, Response } from "express";
 import prismaClient from "../database";
+import { error } from "console";
 
 export class FindGradeController {
     async handle(request: Request, response: Response) {
+        try {
+            const { id } = request.params;
+            const grade = await prismaClient.grade.findFirst({
+                where: {
+                    id: parseInt(id)
+                }
+            })
 
-        const { id } = request.params;
-        const grade = await prismaClient.grade.findFirst({
-            where: {
-                id: parseInt(id)
-            }
-        })
-
-        return response.json(grade)
+            return response.json(grade)
+        }
+        catch {
+            response.status(400).send("Error")
+        }
     }
 }
 
